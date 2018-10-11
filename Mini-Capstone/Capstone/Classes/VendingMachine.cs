da0dd5028522dcd60cb8a416d08c3a83cb4a18d4
Kristin Villalobos <kristin.villalobos@gmail.com>
2018-10-11 15:56:37 -0400

    we almost done it

--- Mini-Capstone/Capstone/Classes/VendingMachine.cs
+++ Mini-Capstone/Capstone/Classes/VendingMachine.cs
@@ -50,37 +50,46 @@ namespace Capstone.Classes
         {
             return items.ToArray();
         }
-
         public void feedMoney()
         {
-            Console.WriteLine();
-            Console.WriteLine("Please enter amount to deposit:\n($1, $2, $5, $10, $20)");
-            int inputAmount = Int32.Parse(Console.ReadLine());
-            switch (inputAmount)
+            try
             {
-                case 1:
-                case 2:
-                case 5:
-                case 10:
-                case 20:
-                    Balance += inputAmount;
-                    Console.WriteLine();
-                    Console.WriteLine($"Amount deposited: ${inputAmount}");
+                Console.WriteLine();
+                Console.WriteLine("Please enter amount to deposit:\n($1, $2, $5, $10, $20)");
+                int inputAmount = Int32.Parse(Console.ReadLine());
+                switch (inputAmount)
+                {
+                    case 1:
+                    case 2:
+                    case 5:
+                    case 10:
+                    case 20:
+                        Balance += inputAmount;
+                        Console.WriteLine();
+                        Console.WriteLine($"Amount deposited: ${inputAmount}");
 
-                    string logFilePath = @"C:\VendingMachine";
-                    string logFileName = "TransactionLog.csv";
-                    string logFullPath = Path.Combine(logFilePath, logFileName);
-                    using (StreamWriter sw = new StreamWriter(logFullPath, false))
-                    {
-                        string outputString = $"{DateTime.UtcNow}| FEED MONEY |${inputAmount}|${Balance}";
-                    }
-                    break;
+                        string logFilePath = @"C:\VendingMachine";
+                        string logFileName = "TransactionLog.csv";
+                        string logFullPath = Path.Combine(logFilePath, logFileName);
+                        using (StreamWriter sw = new StreamWriter(logFullPath, true))
+                        {
+                            string outputString = $"{DateTime.UtcNow}| FEED MONEY |${inputAmount}|${Balance}";
+                            sw.WriteLine(outputString);
+                        }
+                        break;
 
-                default:
-                    Console.WriteLine("Please enter a valid amount.");
-                    Console.WriteLine();
-                    feedMoney();
-                    break;
+                    default:
+                        Console.WriteLine("Please enter a valid amount.");
+                        Console.WriteLine();
+                        feedMoney();
+                        break;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("That isn't even a number. Try again.");
+                Console.WriteLine();
+                feedMoney();
             }
         }
 
@@ -89,59 +98,64 @@ namespace Capstone.Classes
             UserInterface userInterface = new UserInterface();
             foreach (VendingMachineItem item in items)
             {
-                if (selection.ToUpper() == item.typeNumber)
+                try
                 {
-                    if (item.itemQuantity == 0)
+                    if (selection.ToUpper() == item.typeNumber)
                     {
-                        Console.WriteLine();
-                        Console.WriteLine("SOLD OUT");
-                        return;
-                    }
-                    if (Balance < item.itemPrice)
-                    {
-                        Console.WriteLine("NOT ENOUGH MONEY, PLEASE INSERT MORE AND TRY AGAIN");
-                    }
-                    else
-                    {
-                        Console.WriteLine();
-                        Console.WriteLine($"Dispensing: {item.itemName}");
+                        if (item.itemQuantity == 0)
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("SOLD OUT");
+                            return;
+                        }
+                        if (Balance < item.itemPrice)
+                        {
+                            Console.WriteLine("NOT ENOUGH MONEY, PLEASE INSERT MORE AND TRY AGAIN");
+                        }
+                        else
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine($"Dispensing: {item.itemName}");
 
-                        if (item.typeNumber.Contains('A'))
-                        {
-                            Console.WriteLine("Crunch Crunch, Yum!");
-                        }
-                        else if (item.typeNumber.Contains('B'))
-                        {
-                            Console.WriteLine("Munch Munch, Yum!");
-                        }
-                        else if (item.typeNumber.Contains('C'))
-                        {
-                            Console.WriteLine("Glug Glug, Yum!");
-                        }
-                        else if (item.typeNumber.Contains('D'))
-                        {
-                            Console.WriteLine("Chew Chew, Yum!");
-                        }
-                        Console.WriteLine();
-                        item.itemQuantity -= 1;
-                        Balance -= item.itemPrice;
+                            if (item.typeNumber.Contains('A'))
+                            {
+                                Console.WriteLine("Crunch Crunch, Yum!");
+                            }
+                            else if (item.typeNumber.Contains('B'))
+                            {
+                                Console.WriteLine("Munch Munch, Yum!");
+                            }
+                            else if (item.typeNumber.Contains('C'))
+                            {
+                                Console.WriteLine("Glug Glug, Yum!");
+                            }
+                            else if (item.typeNumber.Contains('D'))
+                            {
+                                Console.WriteLine("Chew Chew, Yum!");
+                            }
+                            Console.WriteLine();
+                            item.itemQuantity -= 1;
+                            Balance -= item.itemPrice;
 
-                        string logFilePath = @"C:\VendingMachine";
-                        string logFileName = "TransactionLog.csv";
-                        string logFullPath = Path.Combine(logFilePath, logFileName);
-                        using (StreamWriter sw = new StreamWriter(logFullPath, false))
-                        {
-                            string outputString = $"{DateTime.UtcNow}|{item.itemName}|{item.typeNumber}|${Balance + item.itemPrice}|${Balance}";
+                            string logFilePath = @"C:\VendingMachine";
+                            string logFileName = "TransactionLog.csv";
+                            string logFullPath = Path.Combine(logFilePath, logFileName);
+                            using (StreamWriter sw = new StreamWriter(logFullPath, true))
+                            {
+                                string outputString = $"{DateTime.UtcNow}|{item.itemName}|{item.typeNumber}|${Balance + item.itemPrice}|${Balance}";
+                                sw.WriteLine(outputString);
+                            }
                         }
                     }
                 }
-                else
+                catch(Exception ex)
                 {
                     Console.WriteLine("NO SUCH ITEM EXISTS; PLEASE MAKE A VALID CHOICE!!!");
                     Console.WriteLine();
                     return;
                 }
             }
+
         }
 
         public void MakeChange()
@@ -151,10 +165,10 @@ namespace Capstone.Classes
             decimal dm = 0;
             decimal nk = 0;
 
-            qt = Balance / .25M; 
+            qt = Balance / .25M;
             Balance %= .25M;
             dm = Balance / .10M;
-            Balance  %= .10M;
+            Balance %= .10M;
             nk = Balance / .05M;
             Balance %= .05M;
 
@@ -163,9 +177,10 @@ namespace Capstone.Classes
             string logFilePath = @"C:\VendingMachine";
             string logFileName = "TransactionLog.csv";
             string logFullPath = Path.Combine(logFilePath, logFileName);
-            using (StreamWriter sw = new StreamWriter(logFullPath, false))
+            using (StreamWriter sw = new StreamWriter(logFullPath, true))
             {
                 string outputString = $"{DateTime.UtcNow}| GIVE CHANGE |${beginningBalance}|${Balance}";
+                sw.WriteLine(outputString);
             }
         }
     }
